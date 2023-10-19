8f494d6dc156e6559bdc5e2f707bd94658a309c5
tkhuyn <tkhuyen1212@gmail.com>
2023-10-19 13:15:44 +0700

    change logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <img
-      src="https://inkythuatso.com/uploads/thumbnails/800/2021/11/logo-neu-inkythuatso-01-09-10-41-01.jpg"
-      alt=""
-      class="w-[60px] h-[60px] object-cover" />
+    <img src="/logo.svg" alt="" class="w-[60px] h-[60px] object-cover" />
   </div>
   <div class="flex items-center gap-4">
     <img src="/icon-search.svg" alt="" class="w-8 h-8 cursor-pointer" />
